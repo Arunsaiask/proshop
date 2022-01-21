aa4cc0f397600686209e203bb3ef34994a7524d0
Arunsaiaskgit config --global user.email nanipatel9@gmail.com <nanipatel9@gmail.com>
2022-01-21 21:19:51 +0530

    created init in root of proshop and mkdir backend and inside tht mkdir data,server.js and made routes

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PORT = process.env.PORT || 5000
+const products = require("./data/products")
+
+
+const app = express()
+
+
+
+app.get("/",(req,res)=>{
+  res.send("api is running")
+})
+
+app.get("/api/products",(req,res)=>{
+   res.json(products)
+ })
+ app.get("/api/products/:id",(req,res)=>{
+   const product = products.find((p)=> p._id === req.params.id) 
+   res.json(product)
+  })
+ 
+
+
+app.listen(PORT,(req,res)=>{
+    console.log("Server is live at http://localhost:5000")
+})
